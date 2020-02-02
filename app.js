13debadcdf6a469601d61a9cc5b5f724f9a56e6c
Dan Bartlett <danbart909@gmail.com>
2020-02-02 14:34:32 -0500

    slight changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const cors = require('cors')
+const { NODE_ENV, API_TOKEN } = require('./config')
 
 const app = express()
 
@@ -15,9 +15,16 @@ app
   .use(morgan(morganOption))
   .use(helmet())
   .use(cors())
+  .use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next() // move to the next middleware
 })
 
 app.use(function errorHandler(error, req, res, next) {
@@ -28,6 +35,10 @@ app.use(function errorHandler(error, req, res, next) {
       response = { message: error.message, error }
   }
   res.status(500).json(response)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
 })
 
 module.exports = app
