2c32e1ac8c6c2d57de0ff0a808732538a678dea7
Dan Bartlett <danbart909@gmail.com>
2020-02-02 13:09:59 -0500

    error handler

--- app.js
+++ app.js
@@ -25,18 +25,7 @@ app.use(function errorHandler(error, req, res, next) {
   if (NODE_ENV === 'production') {
     response = { error: { message: 'server error' } }
     } else {
-      console.error(error)
       response = { message: error.message, error }
-  }
-  res.status(500).json(response)
-})
-
-app.use((error, req, res, next) => {
-  let response
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }}
-  } else {
-    response = { error }
   }
   res.status(500).json(response)
 })
