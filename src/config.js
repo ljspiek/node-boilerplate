f6070514ed165f2380983c003bdca6c3b5456386
Dan Bartlett <danbart909@gmail.com>
2020-03-01 19:07:19 -0500

    .

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 0000,
-  DB_URL: process.env.DB.URL || 'postgresql:postgres:401923@localhost/*db*',
-  TEST_DB_URL: process.env.TEST.DB.URL || 'postgresql:postgres:401923@localhost/*test-db*'
+  DATABASE_URL: process.env.DATABASE.URL || 'postgresql:postgres:401923@localhost/*database*',
+  TEST_DATABASE_URL: process.env.TEST.DATABASE.URL || 'postgresql:postgres:401923@localhost/*test-database*'
 }
